c58d5f112d1bfb3ad2783831208971303883493d
Ikey Doherty <ikey@solus-project.com>
2016-12-07 05:02:13 +0000

    manager: Add skeleton for the Pool implementation
    The Pool will be used to track assets shared between the repositories,
    allowing them to deduplicate the storage. A key advantage of this is
    obviously saving on disk space, but it also allows the package data
    to be deduplicated between the repositories too, paving the way
    for a "pull" system.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/manager/pool.go
@@ -0,0 +1,55 @@
+//
+// Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package manager
+
+import (
+	"github.com/boltdb/bolt"
+	"libeopkg"
+)
+
+//
+// A PoolEntry is the main storage area for the actual package information
+// within binman.
+// It is the place where package information is actually stored, the repos
+// only have a linked relationship to the packages.
+type PoolEntry struct {
+	Name     string            // Basename of the package, including suffix
+	Path     string            // Absolute path to the package file
+	RefCount int               // Number of times duplicated
+	Metadata libeopkg.Metadata // Package information for this file
+}
+
+//
+// A Pool is responsible for caching and inserting packages into the filesystem.
+//
+// The main goal is to facilitate deduplication, by storing .eopkg's in a single
+// pool tree.
+// When a pool asset is stored, the asset is then hard-linked into the repository's
+// own tree.
+//
+type Pool struct {
+	// private
+	db *bolt.DB
+}
+
+// NewPool will return a new pool system. This is used primarily by Manager
+// to assist in controlling the repositories.
+func NewPool(db *bolt.DB) *Pool {
+	return &Pool{
+		db: db,
+	}
+}
