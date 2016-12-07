cbc0913d568ea215c7655556a9a61fadd923bb85
Ikey Doherty <ikey@solus-project.com>
2016-12-07 05:57:58 +0000

    util: Add method for determining the base path of a package
    This is a slight departure from binman.py, in that we now force a
    lower case source name. Browing through packages.solus-project.com
    gives you a good idea of why we need to do this..
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/manager/util.go
+++ src/manager/util.go
@@ -18,7 +18,10 @@ package manager
 
 import (
 	"io"
+	"libeopkg"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 // CopyFile will copy the file and permissions to the new target
@@ -49,4 +52,33 @@ func CopyFile(source, dest string) error {
 	// If it fails, meh.
 	os.Chtimes(dest, st.ModTime(), st.ModTime())
 	return nil
+}
+
+//
+// FormPackageBasePath will return the appropriate path base for
+// a package file to live in. In binman, we store packages with
+// a file scheme similar to that of Debian. In all cases we use
+// a lowercase source name, to prevent having "n", "N" directories.
+//
+//      $sourceFirstletter/$sourceName/$pkgfile
+//      i.e:
+//      g/glibc/glibc-2.24-35-1-x86_64.eopkg
+//      g/glibc/glibc-32bit-2.24-35-1-x86_64.eopkg
+//
+// Special mutation is applied to names beginning with "lib", in that
+// we shop off the first 4 letters.
+//
+//      libj/libjpeg-turbo/libjpeg-turbo-1.4.0-5-1-x86_64.eopkg
+//
+// This does also capture *non* library packages, such as LibreOffice,
+// however it enforces a decent level of distribution of the package
+// files among directories, which makes it much easier to navigate.
+// It's also a pain to look at several thousand packages in a single
+// directory..
+func FormPackageBasePath(meta *libeopkg.Metadata) string {
+	source := strings.ToLower(meta.Source.Name)
+	if strings.HasPrefix(source, "lib") && len(source) > 3 {
+		return filepath.Join(source[:4], source)
+	}
+	return filepath.Join(source[0:1], source)
 }
