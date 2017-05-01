f5a89c90e0a2c1dcbbd366c442542060200a6b80
Ikey Doherty <ikey@solus-project.com>
2017-05-01 18:04:08 +0100

    slip: Start adding the public API functions
    This api.go entry point is the main manager wrapper functions that ferryd
    will expose through handlers, and the methods which ultimately the main
    ferry client will interact with.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/ferry/slip/api.go
@@ -0,0 +1,54 @@
+//
+// Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package slip
+
+import (
+	"github.com/boltdb/bolt"
+)
+
+// This file provides the public API functions which are used by ferryd
+// and exposed through it's handlers.
+
+// CreateRepo will request the creation of a new repository
+func (m *Manager) CreateRepo(id string) error {
+	err := m.db.Update(func(tx *bolt.Tx) error {
+		_, err := m.repo.CreateRepo(tx, id)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+// GetRepo will grab the repository if it exists
+// Note that this is a read only operation
+func (m *Manager) GetRepo(id string) (*Repository, error) {
+	var repo *Repository
+	err := m.db.View(func(tx *bolt.Tx) error {
+		r, err := m.repo.GetRepo(tx, id)
+		if err != nil {
+			return err
+		}
+		repo = r
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
