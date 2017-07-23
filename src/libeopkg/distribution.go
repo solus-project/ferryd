88bff69458e75c5daa75f519a3c12e97e5f9d9cb
Ikey Doherty <ikey@solus-project.com>
2017-07-23 17:38:05 +0100

    libeopkg: Add support for distribution.xml
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/libeopkg/distribution.go
@@ -0,0 +1,54 @@
+//
+// Copyright © 2016-2017 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package libeopkg
+
+import (
+	"encoding/xml"
+	"os"
+)
+
+// A Distribution as seen through the eyes of XML
+type Distribution struct {
+	SourceName string // Name of source to match source repos
+	Version    string // Published version number for compatibility
+
+	// Translated description
+	Description []struct {
+		Value string `xml:",cdata"`
+		Lang  string `xml:"lang,attr,omitempty"`
+	}
+
+	Type       string // Type of repository (should always be main, really. Just descriptive)
+	BinaryName string // Name of the binary repository
+
+	Obsoletes []string `xml:"Obsoletes>Package"` // Package names that are no longer supported
+}
+
+// NewDistribution will load the Distribution data from the XML file
+func NewDistribution(xmlfile string) (*Distribution, error) {
+	fi, err := os.Open(xmlfile)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+	dist := &Distribution{}
+	dec := xml.NewDecoder(fi)
+	if err = dec.Decode(dist); err != nil {
+		return nil, err
+	}
+	return dist, nil
+}
---
+++ src/libeopkg/distribution_test.go
@@ -0,0 +1,60 @@
+//
+// Copyright © 017 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package libeopkg
+
+import (
+	"testing"
+)
+
+const (
+	distTestFile = "testdata/distribution.xml"
+)
+
+func TestDistribution(t *testing.T) {
+	dist, err := NewDistribution(distTestFile)
+	if err != nil {
+		t.Fatalf("Failed to load good file: %s", err)
+	}
+	if dist == nil {
+		t.Fatalf("Failed to get distribution")
+	}
+	if dist.SourceName != "Solus" {
+		t.Fatalf("Invalid source name %s", dist.SourceName)
+	}
+	if dist.Version != "1" {
+		t.Fatalf("Invalid version %s", dist.Version)
+	}
+	if len(dist.Description) != 23 {
+		t.Fatalf("Invalid number of descriptions: %d", len(dist.Description))
+	}
+	if dist.Description[0].Lang != "" {
+		t.Fatalf("First element should not have language: %s", dist.Description[0].Lang)
+	}
+	if dist.Description[22].Lang != "zh_CN" {
+		t.Fatalf("Lang on last element is wrong: %s", dist.Description[21].Lang)
+	}
+	if dist.Type != "main" {
+		t.Fatalf("Invalid repo type: %s", dist.Type)
+	}
+	if dist.BinaryName != "Solus" {
+		t.Fatalf("Invalid binary name: %s", dist.BinaryName)
+	}
+
+	if dist.Obsoletes[0] != "pcre" {
+		t.Fatalf("Invalid first obsolete: %s", dist.Obsoletes[0])
+	}
+}
